744b7b65494ca922cf8c14936c19b36bc7ed55fb
João Pedro <jpmrc49@gmail.com>
2024-12-20 12:41:36 -0300

    chore: removes unuse css files

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -2,7 +2,6 @@ import React, { FormEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import api from '../../services/api'
 import { useAuth } from '../../context/AuthContext'
-import './Login.css'
 import {
   Card,
   CardHeader,
